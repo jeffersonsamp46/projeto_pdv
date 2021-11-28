7f8063368ce7502779bc99e08c4bfa4b17607d8a
jeffersonsamp46 <jeffersonsamp@gmail.com>
2021-11-27 23:59:46 -0300

    criando a tela de menu

--- Pdv.dpr
+++ Pdv.dpr
@@ -2,13 +2,15 @@ program Pdv;
 
 uses
   Vcl.Forms,
-  Login in 'Login.pas' {Form1};
+  Login in 'Login.pas' {FrmLogin},
+  Menu in 'Menu.pas' {frmMenu};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TFrmLogin, FrmLogin);
+  Application.CreateForm(TfrmMenu, frmMenu);
   Application.Run;
 end.
